9f844983d9e83db2369daf6e022a1f8abcd406d4
adriell <jelaiadriell16@gmail.com>
2015-06-12 23:24:26 +0800

    next boundaries

--- slither.py
+++ slither.py
@@ -13,7 +13,9 @@ gameExit = False
 
 lead_x = 300
 lead_y = 300
+
 lead_x_change = 0
+lead_y_change = 0
 
 clock = pygame.time.Clock()
 
@@ -22,14 +24,28 @@ while not gameExit:
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                gameExit = True
+
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
+                lead_y_change = 0
+
+            elif event.key == pygame.K_UP:
+                lead_y_change = -10
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
+                lead_y_change = 10
+                lead_x_change = 0
 
 
 
     lead_x += lead_x_change
+    lead_y += lead_y_change
+
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
     pygame.display.update()
